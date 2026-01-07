88669a8c03a2ca54e31bbbb3b322820b4587e2ae
Thomas Farstrike <thomasverstreken@protonmail.com>
2026-01-07 14:40:06 +0100

    Add rlottie

--- lib/lv_conf.h
+++ lib/lv_conf.h
@@ -17,7 +17,7 @@
     #define MICROPY_FFMPEG  0 // works on unix/desktop but not on esp32 (conflicting time.h time_t in /home/user/sources/lvgl_micropython/lib/esp-idf/components/mbedtls/mbedtls/include/mbedtls/platform_time.h)
 #endif
 #ifndef MICROPY_RLOTTIE
-    #define MICROPY_RLOTTIE  0
+    #define MICROPY_RLOTTIE  1
 #endif
 #ifndef MICROPY_TINY_TTF
     #define MICROPY_TINY_TTF  0
